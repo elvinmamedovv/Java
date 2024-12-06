1f0b8e476cf7cc193d8d4aaa5f035113bbd854e0
elvinmamedovv <mammadovv.elvin@gmail.com>
2024-12-07 00:59:56 +0400

    try-cath

--- src/Main.java
+++ src/Main.java
@@ -7,34 +7,20 @@ import java.util.Set;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Map<Integer, String> map = new HashMap<>();
-        map.put(12345, "Elvin");
-        map.put(54321, "Kanan");
-        map.put(67890, "Asif");
-
-
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Identifikasiya nomresini daxil edin: ");
-        int a = scanner.nextInt();
-        if (a == 12345) {
-            System.out.println("Istirakcii: " + map.get(12345));
-        } else if (a == 54321) {
-            System.out.println("Istirakcii: " + map.get(54321));
-        } else if (a == 67890) {
-            System.out.println("Istirakcii: " + map.get(67890));
-        } else {
-            System.out.println("Istirakci tapilmadi");
-        }
 
 
-        Set<Map.Entry<Integer, String>> entrySet = map.entrySet();
-        for (Map.Entry<Integer, String> entry : entrySet) {
+        try {
+            System.out.println("Birinci ededi daxil edin: ");
+            int a = scanner.nextInt();
+            System.out.println("Ikinci ededi daxil edin: ");
+            int b = scanner.nextInt();
 
-            System.out.print(entry.getKey() + " ");
-            System.out.println(entry.getValue() + " ");
+            System.out.println(a / b);
+        } catch (ArithmeticException exception) {
+            System.out.println("Emeliyyati icra etmek mumkun deyil.");
 
         }
-
 
     }
 }
